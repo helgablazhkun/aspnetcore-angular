f2647b1fce52e3ad4fced9b7826a00c520ec73b3
olga-torkhanova <olga.torkhanova@returnonintelligence.com>
2019-07-07 16:59:57 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcore_angular
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = WebHost.CreateDefaultBuilder(args)
+            .Build();
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+             .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.SetBasePath(Directory.GetCurrentDirectory());
+                config.AddJsonFile("hosting.json", optional: true);
+            })
+             .UseStartup<Startup>();
+    }
+}
